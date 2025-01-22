05bb5716b5238dbb3ec26e1c86cafbe5ba9e23ab
Mohamed1407 <127685077+Mohamedademm@users.noreply.github.com>
2025-01-22 23:40:27 +0100

    Add files via upload

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">BEN AMARA Mohamed </span>
+            from <span className="purple"> Monastir, Tunisia.</span>
+            <br />
+            I study computer system development at ISET Sousse and will graduate in this year.
+            <br />
+             I’m skilled in programming, web development, and IT support, with expertise in C, C++, Java, Python, and SQL.
+              I’m passionate about AI, big data, and competitive programming. 😊
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> trying new experiences
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiVisualstudiocode,
+  SiPostman,
+  SiLinux,
+  SiGit,
+  SiFigma,
+  SiAdobephotoshop,
+  SiAdobepremierepro,
+  SiJira, // Added Jira icon
+} from "react-icons/si";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiLinux />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFigma />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAdobephotoshop />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAdobepremierepro />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiJira /> {/* Added Jira icon */}
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
